ca137f999e19dd2d4bf2590f75bd12cb6733397b
Arinis <arinis@bk.ru>
2022-12-22 13:09:21 +0300

    Start

---
+++ src/Cat/Main.java
@@ -0,0 +1,16 @@
+package Cat;
+
+import Cat.Cat;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat bigCat = new Cat(5500, "Murka", 2, "Black");
+        Cat smallCat = new Cat(1000, "Kuzya", 1, "White");
+        bigCat.setName("Pushok");
+        bigCat.setAge(15);
+        System.out.println("The name of big cat is " + bigCat.getName());
+        System.out.println("The name of small cat is " + smallCat.getName());
+        System.out.println("Age " + bigCat.getAge());
+    }
+
+}
---
+++ src/Strings/Main.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class Main {
+    public static void main(String[] args) {
+        Separator test = new Separator(new int[]{2,4,7,8,12,15});
+        test.even();
+    }
+}
