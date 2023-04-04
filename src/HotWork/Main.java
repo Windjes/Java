4491d2dabec1095bb7071cbc2d99ce7eeab81498
Windjes <121225300+Windjes@users.noreply.github.com>
2023-04-04 21:17:49 +0300

    Delete src/HotWork directory

--- src/HotWork/Main.java
+++
@@ -1,8 +0,0 @@
-package Strings;
-
-public class Main {
-    public static void main(String[] args) {
-        Separator test = new Separator(new int[]{2,4,7,8,12,15});
-        test.even();
-    }
-}
